ea080645023fee560f588ca712a079a95bf7ffae
Lashkevich Ryan <lashkevichrain@gmail.com>
2021-09-12 22:28:58 +0700

    Basic data access structure assembly added

---
+++ Playing.DistributedWeb/Web.DataAccess/Interfaces/ISampleMessageRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Web.MessagingModels;
+
+namespace Web.DataAccess.Interfaces
+{
+	public interface ISampleMessageRepository
+	{
+		Task InsertNewMessage(SampleMessage newMessage);
+
+		Task<int> GetLastSessionId();
+	}
+}
---
+++ Playing.DistributedWeb/Web.DataAccess/Repositories/MariaDbRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Web.DataAccess.Interfaces;
+using Web.MessagingModels;
+
+namespace Web.DataAccess.Repositories
+{
+	public class MariaDbRepository : ISampleMessageRepository
+	{
+		public MariaDbRepository(string connectionString)
+		{
+
+		}
+
+		public Task<int> GetLastSessionId()
+		{
+
+			return Task.FromResult(int.MinValue);
+		}
+
+		public Task InsertNewMessage(SampleMessage newMessage)
+		{
+			if (newMessage is null)			
+				throw new ArgumentNullException(nameof(newMessage));
+
+			return Task.CompletedTask;
+		}
+	}
+}
