345f26b52860b23e45b16bbe509ad87ede13ea9d
Lashkevich Ryan <lashkevichrain@gmail.com>
2021-09-12 13:53:32 +0700

    Basic solution structure, messaging model

---
+++ Playing.DistributedWeb/Web.MessagingModels/Models/SampleMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Web.MessagingModels
+{
+	public class SampleMessage
+	{
+		public int Id { get; set; }
+
+		public int SessionId { get; set; }
+
+		public DateTime? NodeOne_Timestamp { get; set; }
+
+		public DateTime? NodeTwo_Timestamp { get; set; }
+
+		public DateTime? NodeThree_Timestamp { get; set; }
+
+		public DateTime? End_Timestamp { get; set; }
+	}
+}
