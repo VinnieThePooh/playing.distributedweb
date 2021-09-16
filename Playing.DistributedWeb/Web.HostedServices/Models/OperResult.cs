b494c3cefbf5e5e626d883b23520e6ab6505afdd
Lashkevich Ryan <lashkevichrain@gmail.com>
2021-09-16 15:55:46 +0700

    OperResult for IWebSocketClientService public methods

---
+++ Playing.DistributedWeb/Web.HostedServices/Models/OperResult.cs
@@ -0,0 +1,25 @@
+﻿namespace Web.HostedServices.Models
+{
+	public struct OperResult
+	{
+		public OperResult(string message)
+		{
+			ResultMessage = message;
+		}
+
+		public bool IsSucceeded => ResultMessage is not null;
+
+		public string ResultMessage { get; }
+
+		public static OperResult Succeeded => new OperResult();
+		
+		public static class Messages
+		{
+			public const string WaitingForGracefulClose = "Waiting for graceful close";
+
+			public const string SendingData = "Still sending data";
+
+			public const string AlreadyStopped = "Service already stopped";
+		}
+	}
+}
